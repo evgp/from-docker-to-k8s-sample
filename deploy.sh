5c9c907c0fc59cd07193fe95780272edb3f35378
Evgeniy Polyarush <bratikha@gmail.com>
2019-04-19 12:35:57 +1200

    .

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
-docker build -t eugocker/multi-client -f ./client/Dockerfile ./client
-docker build -t eugocker/multi-server -f ./server/Dockerfile ./server
-docker build -t eugocker/multi-worker -f ./worker/Dockerfile ./worker
-docker push eugocker/multi-client
-docker push eugocker/multi-server
-docker push eugocker/multi-worker
+docker build -t eugocker/multi-client:latest -t eugocker/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t eugocker/multi-server:latest -t eugocker/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t eugocker/multi-worker:latest -t eugocker/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push eugocker/multi-client:latest
+docker push eugocker/multi-server:latest
+docker push eugocker/multi-worker:latest
+docker push eugocker/multi-client:$SHA
+docker push eugocker/multi-server:$SHA
+docker push eugocker/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=eugocker/multi-server # it must be latest version, so the uniq version setting is required!
-
+kubectl set image deployments/server-deployment server=eugocker/multi-server:$SHA # it must be latest version, so the uniq version setting is required!
+kubectl set image deployments/client-deployment client=eugocker/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=eugocker/multi-worker:$SHA
