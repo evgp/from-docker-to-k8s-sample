d5f53e7f4559cfc04f8cd43e506b4d5989223e49
Evgeniy Polyarush <bratikha@gmail.com>
2019-04-21 22:42:10 +1200

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator version 2</h1>
-            <Link to="/">Home</Link>
+            <Link to="/">Fib calculator ver 2</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
